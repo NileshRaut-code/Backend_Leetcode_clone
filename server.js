d61b388a8eac79f9828c321bf486528dfecee21e
Nilesh Raut <82454856+NileshRaut-code@users.noreply.github.com>
2025-09-30 15:24:24 +0530

    Update server.js
    asdad

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json({ limit: "16kb" }));
 
 app.use(
     cors({
-      origin: process.env.CORS_ORIGIN || "http://localhost:3001",
+      origin: process.env.CORS_ORIGIN ,
       credentials: true,
     })
   );
